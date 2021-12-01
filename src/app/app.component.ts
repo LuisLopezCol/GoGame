8f8a4bc909bbdfb7ba688ed07b8b25701ba2566d
fercholo0730 <fercholo0730@hotmail.com>
2021-12-01 14:12:29 -0500

    dinamically change the rating and the price since the info is not in the API. 404 page included. cart desing included

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'personalPortfolio';
+  constructor(private renderer2:Renderer2){}
+
+  //----------------------Show $ Hide cart----------------------
+  @ViewChild("cartOverlay") cartOverlay!: ElementRef;
+  @ViewChild("cartDom") cartDom!: ElementRef;
+            ShowCart(){
+              const cartOverlay = this.cartOverlay.nativeElement;
+              this.renderer2.addClass(cartOverlay,"transparentBcg");
+              const cartDom = this.cartDom.nativeElement;
+              this.renderer2.addClass(cartDom,"showCart");
+            }
+            HideCart(){
+              const cartOverlay = this.cartOverlay.nativeElement;
+              this.renderer2.removeClass(cartOverlay,"transparentBcg");
+              const cartDom = this.cartDom.nativeElement;
+              this.renderer2.removeClass(cartDom,"showCart");
+            }
 }
